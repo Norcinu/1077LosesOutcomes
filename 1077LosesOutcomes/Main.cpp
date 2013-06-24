f0287b77dcb19a05b46de5a871efbe9624d27e55
Steven Gleed <ledbybyson@gmail.com>
2013-06-24 23:02:01 +0100

    Moved on

--- 1077LosesOutcomes/Main.cpp
+++ 1077LosesOutcomes/Main.cpp
@@ -7,7 +7,11 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	const string filename = "presearch.txt";
+#ifdef __APPLE__
+	const string filename = "/Users/Steven/Documents/code/json/presearch.txt";
+#else
+    const string filename = "presearch.txt";
+#endif
 	ifstream search_file(filename);
 	string line = "";
 	array<string, 4672> search_contents;
@@ -32,7 +36,8 @@ int main(int argc, char **argv)
 	five_or_more.fill("");
 	int feat_symbol_count = 0;
 	int cnt = 0;
-	array<int, 4762> less_than_positions = {-1};
+	array<int, 4762> less_than_positions;
+    less_than_positions.fill(0);
 	
 	for (auto it = search_contents.begin(); it != search_contents.end(); it++)
 	{
@@ -57,10 +62,39 @@ int main(int argc, char **argv)
 		feat_symbol_count = 0;
 		counter++;
 	}
+    
+#ifdef __APPLE__
+    const string output_file = "/Users/Steven/Documents/code/json/outcomes.h";
+#else
+   const string output_file = "outcomes.h";
+#endif
 
-	ofstream output("outcomes.h");
-
-	//for (auto it = 
+	ofstream output(output_file);
+    for (auto s = search_contents.begin(); s != search_contents.end(); s++)
+    {
+        auto comp = feature_strings.begin();
+		for (;comp != feature_strings.end(); comp++)
+		{
+			if (string::npos != (*s).find((*comp)))
+			{
+				feat_symbol_count++;
+			}
+		}
+		
+		if (feat_symbol_count < 3)
+		{
+            auto num = random() % 2 + 1;
+			if (num == 2)
+            {
+                *s = five_or_more[random() % five_or_more.size()-1];
+                cnt++;
+                output << *s << endl;
+            }
+		}
+        feat_symbol_count = 0;
+    }
+    output.close();
+	//for (auto it =
 
 	return 0;
 }
